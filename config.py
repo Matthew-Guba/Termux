4ddad22317f6e1fda342c6e99ce0598fcd0fdc83
Matvey <chemistrygeorge3@gmail.com>
2026-04-14 14:02:45 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "8487880807:AAFYzh391X6eoxIygvv6F_W45t2JpfxjXTc"
+ALLOWED_USERS = [8139183422]
+PLUGINS_DIR = "plugins"
+MAX_OUTPUT_LENGTH = 4000
+COMMAND_TIMEOUT = 30
+DANGEROUS_COMMANDS = ['rm -rf /', 'mkfs', 'dd if=']
+# ========== USERBOT (для рассылок) ==========
+API_ID = 32023942  # Вставь свой api_id
+API_HASH = "be329bb418af2550625ef4a25627b84f"  # Вставь свой api_hash
+PHONE_NUMBER = "+573185884615"  # Например: "+79123456789"
+SESSION_NAME = "userbot_session"
